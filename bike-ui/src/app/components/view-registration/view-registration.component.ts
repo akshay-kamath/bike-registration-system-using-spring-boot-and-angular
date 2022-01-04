61fe20fe38cc9b3906c01cb0721ec3faea55e928
Akshay Kamath <akshaykamath1996@gmail.com>
2022-01-04 13:26:03 +0530

    Added view registration

---
+++ bike-ui/src/app/components/view-registration/view-registration.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit } from '@angular/core';
+import { BikeService } from 'src/app/services/bike.service';
+import { ActivatedRoute } from '@angular/router';
+import { Observable } from 'rxjs';
+
+
+
+@Component({
+  selector: 'app-view-registration',
+  templateUrl: './view-registration.component.html',
+  styleUrls: ['./view-registration.component.css']
+})
+export class ViewRegistrationComponent implements OnInit {
+
+  public bikeReg: any;
+  constructor(private bikeService:BikeService,private route:ActivatedRoute) { }
+
+  ngOnInit() {
+    this.getBikeReg(this.route.snapshot.params.id);
+  }
+
+  getBikeReg(id:number){
+    this.bikeService.getBike(id).subscribe(
+      data => {
+        this.bikeReg= data;
+      },
+      err => console.error(err),
+      ()=>console.log("bikes loaded")
+    );
+  }
+
+
+}
